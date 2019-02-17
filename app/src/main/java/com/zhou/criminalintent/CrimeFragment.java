d0835e8ab59232284e6b312054ce99f608189c62
ZHOU <prince5460@126.com>
2019-02-17 22:09:10 +0800

    Initial commit

---
+++ app/src/main/java/com/zhou/criminalintent/CrimeFragment.java
@@ -0,0 +1,67 @@
+package com.zhou.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+public class CrimeFragment  extends Fragment {
+    private Crime crime;
+    private EditText mTextField;
+    private Button mDateButton;
+    private CheckBox mSolveCheckBox;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        crime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTextField = view.findViewById(R.id.crime_title);
+        mTextField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                crime.setmTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mDateButton = view.findViewById(R.id.crime_date);
+        mDateButton.setText(crime.getmDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolveCheckBox = view.findViewById(R.id.crime_solved);
+        mSolveCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                crime.setmSolved(isChecked);
+            }
+        });
+
+        return view;
+    }
+}
